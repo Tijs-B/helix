e540aeb3dbc22647bbc5281bd84e9c1f434380f0
Tijs-B <14929648+Tijs-B@users.noreply.github.com>
2025-10-22 13:03:22 +0200

    python docstrings

--- runtime/queries/python/highlights.scm
+++ runtime/queries/python/highlights.scm
@@ -308,3 +308,18 @@
  
 (comment) @comment
 (string) @string
+
+; docstrings
+; Module docstring
+(module . (expression_statement (string) @string.docstring))
+
+; Class docstring
+(class_definition
+  body: (block . (expression_statement (string) @string.docstring)))
+
+; Function/method docstring
+(function_definition
+  body: (block . (expression_statement (string) @string.docstring)))
+
+; Attribute docstring
+((expression_statement (assignment)) . (expression_statement (string) @string.docstring))
